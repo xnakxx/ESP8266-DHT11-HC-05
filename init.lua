56e89e570735b6b815351b4193c86cf24487267b
xnakxx <xnakxx@gmail.com>
2015-03-18 21:00:42 -0700

    Create init.lua
    Make sure you use your SSID and Password.

---
+++ init.lua
@@ -0,0 +1,12 @@
+wifi.setmode(wifi.STATION)
+wifi.sta.config("SSID","PASSWORD")
+wifi.sta.connect()
+tmr.alarm(1, 2000, 1, function() 
+if wifi.sta.getip()== nil then 
+print(".") 
+else 
+tmr.stop(1)
+print("IP is "..wifi.sta.getip())
+end 
+end)
+dofile("server.lc")
